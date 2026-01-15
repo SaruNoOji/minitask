a3fc88d7c550d3b2f07f7a239201ce650b5194a1
Daniils <bloody.skull12332@gmail.com>
2026-01-15 22:07:11 +0200

    added create_user function

--- db/hash.py
+++ db/hash.py
@@ -3,6 +3,7 @@ from passlib.context import CryptContext
 pwd_cxt = CryptContext(schemes='bcrypt', deprecated='auto')
 
 class Hash():
+    
     def bcrypt(password: str):
         return pwd_cxt.hash(password)
 
