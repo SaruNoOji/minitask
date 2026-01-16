eec3f310ea3f8c5a94a9d068857b833b59383c0a
Daniils <bloody.skull12332@gmail.com>
2026-01-16 15:43:13 +0200

    creat schemas for Tasks table

--- schemas.py
+++ schemas.py
@@ -1,6 +1,13 @@
+from datetime import datetime
 from pydantic import BaseModel, EmailStr, Field
 from pydantic import EmailStr, ConfigDict
+from enum import Enum
+from typing import Optional
 
+class TaskStatus(str, Enum):
+    new = "new"
+    in_progress = "in_progress"
+    done = "done"
 
 class UserBase(BaseModel):
 
@@ -14,3 +21,27 @@ class DisplayUser(BaseModel):
     username : str
     email: str
     model_config = ConfigDict(from_attributes=True)
+
+class TasksCreate(BaseModel):
+
+    title : str = Field(min_length=3,max_length=120)
+    description : Optional[str] = Field(None,max_length=1000)
+    status : TaskStatus = TaskStatus.new
+
+class TasksDisplay(BaseModel):
+
+    id: int
+    title: str
+    description : Optional[str]
+    status: TaskStatus
+    user_id : int
+    created_at : datetime
+    updated_at : datetime
+
+    model_config = ConfigDict(from_attributes=True)
+
+class TaskUpdate(BaseModel):
+
+    title: Optional[str] = Field(default=None,min_length=3,max_length=120)
+    description : Optional[str] = Field(default=None,max_length=1000)
+    status : Optional[TaskStatus] = None
