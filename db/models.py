d57ef19aa40de6c9a9125898eee29511daee8ec7
Daniils <bloody.skull12332@gmail.com>
2026-01-16 15:19:40 +0200

    add DbTasks model

--- db/models.py
+++ db/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timezone
 from sqlalchemy.sql.schema import ForeignKey
 from sqlalchemy.sql.sqltypes import String, Integer, DateTime
 from db.database import Base
@@ -6,7 +7,28 @@ from sqlalchemy.orm import relationship
 
 class DbUser(Base):
     __tablename__ = 'users'
-    id = Column(Integer, primary_key=True, index=True )
+
+    id = Column(Integer, primary_key=True)
     username = Column(String, unique=True)
     email = Column (String,unique=True)
     password = Column(String)
+
+    tasks = relationship(
+        "DbTasks",
+        back_populates="user",
+        cascade="all, delete-orphan"
+    )
+
+
+class DbTasks(Base):
+    __tablename__ = 'tasks'
+
+    id = Column(Integer, primary_key=True, index=True )
+    title = Column(String)
+    description = Column (String,nullable=True)
+    status = Column(String)
+    user_id = Column(Integer, ForeignKey('users.id', ondelete="CASCADE"))
+    created_at = Column(DateTime, nullable= False, default=lambda: datetime.now(timezone.utc))
+    updated_at = Column(DateTime, nullable= False, default=lambda: datetime.now(timezone.utc))
+
+    user = relationship("DbUser", back_populates="tasks")
